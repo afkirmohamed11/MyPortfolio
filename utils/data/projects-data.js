a1652c625c16d04467b38d582ac3ed42371a0b2a
afkir <mohamedafkir078@gmail.com>
2024-12-24 16:53:45 +0100

    edited project section

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,16 +1,6 @@
 export const projectsData = [
     {
         id: 1,
-        name: 'Kafka Rental Streaming Data Pipeline using Kafka and Terraform',
-        description: "I built a real-time data pipeline for processing and analyzing Moroccan rental data. This pipeline extracts data from a PostgreSQL database, streams it through Apache Kafka, stores it in Amazon S3, and then makes it available for SQL-based analysis using Amazon Athena. The infrastructure was provisioned using Terraform.",
-        tools: ['Python', 'Kafka', 'Terraform', 'PostgreSQL', 'Amazon EC2', 'Amazon S3', 'AWS Glue Data Catalog', 'Amazon Athena'],
-        role: 'Data Engineer',
-        code: '',
-        demo: '',
-        github_link: 'https://github.com/afkirmohamed11/Kafka_Rental_Streaming_Data_Pipeline.git'
-    },
-    {
-        id: 2,
         name: 'Property Rental Price Prediction App',
         description: "My team and I built a comprehensive system for predicting apartment prices in Morocco. I performed web scraping from a Moroccan website and preprocessed the data using Python. I developed a hybrid regression model using Gradient Boosting, Random Forest, and Lasso algorithms to predict apartment prices across ten Moroccan cities. Additionally, I built a personalized recommendation system and created a user interface using Flask, HTML, CSS, and JavaScript.",
         tools: ['Python', 'Gradient Boosting', 'Random Forest', 'Lasso', 'Web Scraping', 'Flask', 'HTML', 'CSS', 'JavaScript'],
@@ -20,27 +10,17 @@ export const projectsData = [
         github_link: 'https://github.com/afkirmohamed11/RentalHousingPricePrediction.git'
     },
     {
+        id: 2,
+        name: 'Intelligent Word Spell Checker using NLP and N-gram Language Models',
+        description: "I built an intelligent spell checker that leverages Natural Language Processing and n-gram language models to detect and correct spelling errors. The system combines advanced NLP techniques for text processing with statistical language patterns to understand word context. By analyzing both word similarity and contextual probability, it provides accurate spelling suggestions that maintain semantic coherence in sentences.",
+        tools: ['Python', 'Numpy', 'NLTK', 'N-gram Language Models'],
+        role: 'Data Scientist',
+        code: '',
+        demo: '',
+        github_link: 'https://github.com/afkirmohamed11/N-gram-Based-Smart-Spell-Corrector.git'
+    },
+    {
         id: 3,
-        name: 'Agricultural Patent Analysis Data Pipeline',
-        description: "My team and I built a comprehensive patent data processing pipeline. We extracted patent data from multiple sources, including Google Patents and CIPO, and stored it in MongoDB, serving as a data lake solution. We transformed the data using Spark and loaded it into a PostgreSQL data warehouse. The data was then visualized and analyzed using Power BI. We also built a user interface using Flask, JavaScript, CSS, and HTML.",
-        tools: ['Python', 'MongoDB', 'Power BI', 'Spark', 'PostgreSQL', 'Flask', 'JavaScript', 'CSS', 'HTML'],
-        role: 'Data Engineer',
-        code: '',
-        demo: '',
-        github_link: 'https://github.com/afkirmohamed11/PatentDataAnalysisPipeline.git'
-    },
-    {
-        id: 4,
-        name: 'ETL Pipeline Automated with Airflow',
-        description: "I developed an Airflow pipeline for data warehousing. I created a pipeline that periodically extracts data from a MySQL staging area, transforms it with timestamps, and loads it into a PostgreSQL data warehouse.",
-        tools: ['Python', 'Airflow', 'MySQL', 'PostgreSQL'],
-        role: 'Data Engineer',
-        code: '',
-        demo: '',
-        github_link: 'https://github.com/afkirmohamed11/Airflow_ETL.git'
-    },
-    {
-        id: 5,
         name: 'Heart Disease Prediction App',
         description: "I developed a heart disease prediction model using machine learning techniques in Python. I deployed the model with Streamlit and Docker to ensure user-friendly access.",
         tools: ['Python', 'Machine Learning', 'Streamlit', 'Docker'],
@@ -50,7 +30,37 @@ export const projectsData = [
         github_link: 'https://github.com/afkirmohamed11/HeartDiseacePredictionApp.git'
     },
     {
+        id: 4,
+        name: 'Kafka Rental Streaming Data Pipeline  using Kafka and Terraform',
+        description: "I built a real-time data pipeline for processing and analyzing Moroccan rental data. This pipeline extracts data from a PostgreSQL database, streams it through Apache Kafka, stores it in Amazon S3, and then makes it available for SQL-based analysis using Amazon Athena. The infrastructure was provisioned using Terraform.",
+        tools: ['Python', 'Kafka', 'Terraform', 'PostgreSQL', 'Amazon EC2', 'Amazon S3', 'AWS Glue Data Catalog', 'Amazon Athena'],
+        role: 'Data Engineer',
+        code: '',
+        demo: '',
+        github_link: 'https://github.com/afkirmohamed11/Kafka_Rental_Streaming_Data_Pipeline.git'
+    },
+    {
+        id: 5,
+        name: 'Agricultural Patent Analysis Data Pipeline',
+        description: "My team and I built a comprehensive patent data processing pipeline. We extracted patent data from multiple sources, including Google Patents and CIPO, and stored it in MongoDB, serving as a data lake solution. We transformed the data using Spark and loaded it into a PostgreSQL data warehouse. The data was then visualized and analyzed using Power BI. We also built a user interface using Flask, JavaScript, CSS, and HTML.",
+        tools: ['Python', 'MongoDB', 'Power BI', 'Spark', 'PostgreSQL', 'Flask', 'JavaScript', 'CSS', 'HTML'],
+        role: 'Data Engineer',
+        code: '',
+        demo: '',
+        github_link: 'https://github.com/afkirmohamed11/PatentDataAnalysisPipeline.git'
+    },
+    {
         id: 6,
+        name: 'ETL pipeline automated with Airflow',
+        description: "I developed an Airflow pipeline for data warehousing. I created a pipeline that periodically extracts data from a MySQL staging area, transforms it with timestamps, and loads it into a PostgreSQL data warehouse.",
+        tools: ['Python', 'Airflow', 'MySQL', 'PostgreSQL'],
+        role: 'Data Engineer',
+        code: '',
+        demo: '',
+        github_link: 'https://github.com/afkirmohamed11/Airflow_ETL.git'
+    },
+    {
+        id: 7,
         name: 'Exam Management Application',
         description: "My classmate and I developed an exam management web application using Spring Boot, Spring Security, and Spring Data JPA with a JSP frontend and MySQL database. The application facilitated exam management, optimized administration and teacher management, and organized pedagogical elements. We implemented a three-layered architecture with distinct Presentation, Business Services, and Data Access layers, adhering to the MVC model.",
         tools: ['Java', 'Spring Boot', 'Spring Security', 'Spring Data JPA', 'MySQL', 'JSP'],
@@ -58,16 +68,6 @@ export const projectsData = [
         code: '',
         demo: '',
         github_link: 'https://github.com/afkirmohamed11/SpringBootExamManagamentApp.git'
-    },
-    {
-        id: 7,
-        name: 'Intelligent Word Spell Checker using NLP and N-gram Language Models',
-        description: "I built an intelligent spell checker that leverages Natural Language Processing and n-gram language models to detect and correct spelling errors. The system combines advanced NLP techniques for text processing with statistical language patterns to understand word context. By analyzing both word similarity and contextual probability, it provides accurate spelling suggestions that maintain semantic coherence in sentences.",
-        tools: ['Python', 'Numpy', 'NLTK', 'N-gram Language Models'],
-        role: 'Data Scientist',
-        code: '',
-        demo: '',
-        github_link: 'https://github.com/afkirmohamed11/N-gram-Based-Smart-Spell-Corrector.git'
     },
     {
         id: 8,
