dcae0fbcf90a32e04b4cfa0b0e6a585dea6e7179
afkir <mohamedafkir078@gmail.com>
2024-12-24 16:36:48 +0100

    edited projects

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Afkir Mohamed",
   profile: '/profile.png',
-  designation: "Data Engineer",
+  designation: "Data Engineer & Data Scientist",
   description:"My name is Afkir Mohamed, a dedicated and enthusiastic Data Engineer with a strong passion for data-driven problem-solving. I thrive in transforming raw data into meaningful insights that drive business decisions. With a deep understanding of data pipelines, ETL processes, and database management.I am constantly expanding my toolkit to include the latest technologies in data engineering. I am always eager to learn, explore new methodologies, and apply my skills to make a significant impact. I am open to job opportunities that align with my technical abilities and passion for innovation.",
   email: 'mohamedafkir078@gmail.com',
   address: 'Imzouren, Al Hoceima, Morocco',
